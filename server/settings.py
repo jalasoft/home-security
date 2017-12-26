8efc815100e756838f94c6102faf8ab6581d3715
Honzales <Honzales@raspberry.cz>
2017-12-26 21:51:53 +0000

    one more refinement of configuration

--- server/settings.py
+++ server/settings.py
@@ -74,21 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -119,15 +104,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 from server.config import AppConfig
 
-CONFIG_FILE = "/home/pi/home-alarm/dev/conf/application.yml"
+CONFIG_FILE = BASE_DIR + "/../conf/application.yml"
 config = AppConfig(CONFIG_FILE);
 config.load()
 
